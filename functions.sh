25f835b7e524c58d039828e1c1cb68d2de0367a6
major1201 <major1201@gmail.com>
2023-10-08 16:07:13 +0800

    feat: add identifyIPAddr, isIPv6, isIPAddr

--- functions.sh
+++ functions.sh
@@ -25,6 +25,52 @@ function isIPv4() {
     return 0
 }
 
+function identifyIPAddr() {
+    # Usage: identifyIPAddr "string"
+    #
+    # identifyIPAddr "1.1.1.1" => Ipv4
+    # identifyIPAddr "fd08::1" => Ipv6
+    # identifyIPAddr "1.1.1.1111" => Invalid
+
+    local script=$'
+import sys, os
+from ipaddress import ip_address, IPv4Address, IPv6Address
+try:
+    ip = ip_address(sys.stdin.read().strip())
+    if type(ip) is IPv4Address:
+        print("IPv4")
+    elif type(ip) is IPv6Address:
+        print("IPv6")
+    else:
+        print("Unknown")
+except:
+    print("Invalid")
+'
+    echo "$1" | python3 -c "${script}"
+}
+
+function isIPv6() {
+    # Usage: isIPv6 "string"
+
+    local res
+    res=$(identifyIPAddr "$1")
+    if [[ "${res}" = "IPv6" ]]; then
+        return 0
+    fi
+    return 1
+}
+
+function isIPAddr() {
+    # Usage: isIPAddr "string"
+
+    local res
+    res=$(identifyIPAddr "$1")
+    if [[ "${res}" = "IPv4" || "${res}" = "IPv6" ]]; then
+        return 0
+    fi
+    return 1
+}
+
 function findFreePort() {
     # Usage: findFreePort "fromPort" "toPort"
 
